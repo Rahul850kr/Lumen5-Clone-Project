00e722bf973385414d45a06727b0922bba485a60
Rahul kumar <rahul850kr@gmail.com>
2022-08-23 20:13:44 +0530

    installing necessary dependencies

---
+++ src/components/NavbarAfterLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarAfterLogin = () => {
+  return (
+    <div>NavbarAfterLogin</div>
+  )
+}
+
+export default NavbarAfterLogin
---
+++ src/components/NavbarBeforeLogin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavbarBeforeLogin = () => {
+  return (
+    <div>NavbarBeforeLogin</div>
+  )
+}
+
+export default NavbarBeforeLogin
