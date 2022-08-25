ac57652942db5fcc02fb3dac1eab514a776aac11
manojbarkicodee <manojjunjappabarki@gmail.com>
2022-08-25 17:12:44 +0530

    updated conflit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
 import {BrowserRouter} from "react-router-dom"
